97d81cb379310e2f6af375907ec736a9d8f27ec1
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2024-12-13 17:15:50 +0530

    inital project setup

---
+++ src/test/java/com/starktony/learnspringboot/LearnspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starktony.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
